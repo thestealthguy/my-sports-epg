995c8ca84160edb74f5ce1dc2bbbe9f55b110ae9
thestealthguy <wrgreer@gmail.com>
2026-01-26 21:18:08 -0500

    Create player_api.php

---
+++ player_api.php
@@ -0,0 +1,13 @@
+{
+  "user_info": {
+    "auth": 1,
+    "status": "Active",
+    "exp_date": "1735689600"
+  },
+  "server_info": {
+    "url": "https://your-username.github.io/my-sports-epg/",
+    "port": "443",
+    "https_port": "443",
+    "server_protocol": "https"
+  }
+}
